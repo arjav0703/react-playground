6931903da6ec71df2890f9235bf81af01093a718
Arjav Jain <arjavjain0703@gmail.com>
2025-04-27 12:00:35 +0530

    add userdetail

---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const userpage = () => {
+  return (
+    <>
+      <h1></h1>
+      <div>User Details</div>
+    </>
+  );
+};
+export default userpage;
