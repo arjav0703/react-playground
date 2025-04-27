234bf1b2337440c6959a6969354cc4a6f724b917
Arjav Jain <arjavjain0703@gmail.com>
2025-04-27 12:26:16 +0530

    dynamic routes

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,14 +1,45 @@
 "use client";
 import axios from "axios";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const userpage = () => {
+export default function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchUserDetails() {
+      try {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/users/${id}`,
+        );
+        setUser(response.data);
+      } catch (err) {
+        setError("Failed to fetch user details.");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUserDetails();
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <>
-      <h1></h1>
-      <div>User Details</div>
-    </>
+    <div>
+      <h1>{user?.name}</h1>
+      <p>ID: {user?.id}</p>
+      <p>Email: {user?.email}</p>
+      <p>Phone: {user?.phone}</p>
+    </div>
   );
-};
-export default userpage;
+}
