b6f2c854bd84fb2f1595ec1b48ed631cb9548acb
Arjav Jain <arjavjain0703@gmail.com>
2025-05-01 13:11:13 +0530

    organize

---
+++ 2/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+
+const Home = () => {
+  const [marks, setMarks] = useState(0);
+  return (
+    <>
+      <button></button>
+    </>
+  );
+};
+
+export default Home;
